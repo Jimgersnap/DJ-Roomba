b8936187973cf4e05b30c705e2cac835c94dc004
Jimgersnap <zxirius@live.com>
2019-05-06 15:49:50 -0700

    Added summon on play/stream functionality and new permission defaults
    Bot now automatically joins voice on use of /play and /stream
    if not already on voice if permissions allow.
    
    example_permissions.ini have new defaults

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -42,6 +42,8 @@ from .json import Json
 from .constants import VERSION as BOTVERSION
 from .constants import DISCORD_MSG_CHAR_LIMIT, AUDIO_CACHE_PATH
 
+from typing import Optional
+
 
 load_opus_lib()
 
@@ -408,7 +410,7 @@ class MusicBot(discord.Client):
         # I hope I don't have to set the channel here
         # instead of waiting for the event to update it
 
-    def get_player_in(self, guild:discord.Guild) -> MusicPlayer:
+    def get_player_in(self, guild:discord.Guild) -> Optional[MusicPlayer]:
         return self.players.get(guild.id)
 
     async def get_player(self, channel, create=False, *, deserialize=False) -> MusicPlayer:
@@ -1305,7 +1307,7 @@ class MusicBot(discord.Client):
             )
         return True
 
-    async def cmd_play(self, message, player, channel, author, permissions, leftover_args, song_url):
+    async def cmd_play(self, message, _player, channel, author, permissions, leftover_args, song_url):
         """
         Usage:
             {command_prefix}play song_link
@@ -1319,6 +1321,28 @@ class MusicBot(discord.Client):
         it will use the metadata (e.g song name and artist) to find a YouTube
         equivalent of the song. Streaming from Spotify is not possible.
         """
+
+        if _player:
+            player = _player
+        elif permissions.summonplay:
+            vc = author.voice.channel if author.voice else None
+            response = await self.cmd_summon(channel, channel.guild, author, vc)
+            if self.config.embeds:
+                content = self._gen_embed()
+                content.title = 'summon'
+                content.description = response.content
+            else:
+                content = response.content
+            await self.safe_send_message(
+                channel, content,
+                expire_in=response.delete_after if self.config.delete_messages else 0
+            )
+            player = self.get_player_in(channel.guild)
+
+        if not player:
+            raise exceptions.CommandError(
+                'I am not in a voice channel.  '
+                'Use `%ssummon` to summon me to your voice channel.' % self.config.command_prefix)
 
         song_url = song_url.strip('<>')
 
@@ -1943,7 +1967,7 @@ class MusicBot(discord.Client):
 
         # return Response(reply_text, delete_after=30)
 
-    async def cmd_stream(self, player, channel, author, permissions, song_url):
+    async def cmd_stream(self, _player, channel, author, permissions, song_url):
         """
         Usage:
             {command_prefix}stream song_link
@@ -1953,6 +1977,28 @@ class MusicBot(discord.Client):
         media without predownloading it.  Note: FFmpeg is notoriously bad at handling
         streams, especially on poor connections.  You have been warned.
         """
+
+        if _player:
+            player = _player
+        elif permissions.summonplay:
+            vc = author.voice.channel if author.voice else None
+            response = await self.cmd_summon(channel, channel.guild, author, vc)
+            if self.config.embeds:
+                content = self._gen_embed()
+                content.title = 'summon'
+                content.description = response.content
+            else:
+                content = response.content
+            await self.safe_send_message(
+                channel, content,
+                expire_in=response.delete_after if self.config.delete_messages else 0
+            )
+            player = self.get_player_in(channel.guild)
+
+        if not player:
+            raise exceptions.CommandError(
+                'I am not in a voice channel.  '
+                'Use `%ssummon` to summon me to your voice channel.' % self.config.command_prefix)
 
         song_url = song_url.strip('<>')
 
