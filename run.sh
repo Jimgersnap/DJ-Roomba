914d6e73163352da61750cb955416fc19c405602
Jimgersnap <zxirius@live.com>
2026-05-23 01:52:51 -0700

    Replace MusicBot references with DJ Roomba in run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Assuming no files have been moved, 
-# make sure we're in MusicBot directory...
-cd "$(dirname "${BASH_SOURCE[0]}")" || { echo "Could not change directory to MusicBot."; exit 1; }
+# Assuming no files have been moved,
+# make sure we're in the DJ Roomba directory...
+cd "$(dirname "${BASH_SOURCE[0]}")" || { echo "Could not change directory to DJ Roomba."; exit 1; }
 
 
-# Check if this script is being run on windows and redirect the user.  
+# Check if this script is being run on windows and redirect the user.
 if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] ; then
     echo "run.sh is not for Windows.  Use the run.bat file instead."
     read -rp "Press any key to exit."
@@ -15,7 +15,7 @@ fi
 # provides an exit that also deactivates venv.
 function do_exit() {
     if [ "${VIRTUAL_ENV}" != "" ] ; then
-        echo "Leaving MusicBot Venv..."
+        echo "Leaving DJ Roomba venv..."
         deactivate
     fi
     exit "$1"
@@ -23,12 +23,12 @@ function do_exit() {
 
 # attempt to find the "standard" venv and activate it.
 if [ -f "../bin/activate" ] ; then
-    echo "Detected MusicBot Venv & Loading it..."
+    echo "Detected venv, loading it..."
     # shellcheck disable=SC1091
     source "../bin/activate"
 fi
 
-# Suported versions of python using only major.minor format
+# Supported versions of python using only major.minor format
 PySupported=("3.13" "3.12" "3.11" "3.10")
 
 # compile a list of bin names to try for.
@@ -58,8 +58,6 @@ for PyBin in "${PyBins[@]}" ; do
     fi
     PY_VER_MAJOR=$((PY_VER[0]))
     PY_VER_MINOR=$((PY_VER[1]))
-    # PY_VER_PATCH=$((PY_VER[2]))
-    # echo "run.sh detected $PY_BIN version: $PY_VER_MAJOR.$PY_VER_MINOR.$PY_VER_PATCH"
 
     # Major version must be 3+
     if [[ $PY_VER_MAJOR -ge 3 ]]; then
@@ -74,11 +72,11 @@ done
 
 # if we don't have a good version for python, bail.
 if [[ "$VerGood" == "0" ]]; then
-    echo "Python 3.10 or higher is required to run MusicBot."
+    echo "Python 3.10 or higher is required to run DJ Roomba."
     do_exit 1
 fi
 
-echo "Using '${Python_Bin}' to launch MusicBot..."
+echo "Using '${Python_Bin}' to launch DJ Roomba..."
 # Run python using the bin name we determined via version fetch.
 # We also pass all arguments from this script into python.
 $Python_Bin run.py "$@"
